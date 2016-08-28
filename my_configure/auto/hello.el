dfa199974c16aa07b8c8039bcdab44ac433017f9
hxb <292554331@qq.com>
2016-08-28 17:09:10 +0800

    change name

---
+++ my_configure/auto/hello.el
@@ -0,0 +1,36 @@
+(TeX-add-style-hook
+ "hello"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("ctex" "UTF8" "noindent")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "pgfpages"
+    "latexsym"
+    "pifont"
+    "units"
+    "amsmath"
+    "amsfonts"
+    "amssymb"
+    "marvosym"
+    "graphicx"
+    "booktabs"
+    "ctex"
+    "color"
+    "listings"
+    "CJKutf8")
+   (LaTeX-add-xcolor-definecolors
+    "keywordcolor"
+    "blue"))
+ :latex)
+
