6248c8f789c0d1fb8d0d9bce73e82f917a19f961
hxb <292554331@qq.com>
2016-08-30 14:53:33 +0800

    marked

--- my_configure/auto/hello.el
+++ my_configure/auto/hello.el
@@ -4,13 +4,16 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("ctex" "UTF8" "noindent")))
    (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -29,6 +32,8 @@
     "color"
     "listings"
     "CJKutf8")
+   (TeX-add-symbols
+    '("quotes" 1))
    (LaTeX-add-xcolor-definecolors
     "keywordcolor"
     "blue"))
